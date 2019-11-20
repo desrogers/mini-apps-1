a1bc45118e56d349ee8148347b974dd35ef13220
dezrogers <desmondrogers2@gmail.com>
2019-11-20 13:12:05 -0800

    Set up environment

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,7 @@
 
 
+const App = () => {
+  return <p>Hello World!</p>;
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => console.log(`Server is listening on port 3000`));
