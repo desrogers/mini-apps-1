3f1c35ba18f7673fc8211c3fce809e75b99b676c
Desmond Rogers <desmondrogers2@gmail.com>
2020-07-28 14:19:36 -0500

    Stub basic gameplay methods
    es6ify functions
    
    --amend
    
    Fix return method

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,47 @@
+(function TicTacToe() {
+  const Player = (mark) => {
+    const obj = {};
+    obj.mark = mark;
+    obj.isTurn = false;
+    obj.score = 0;
+    return obj;
+  };
 
+  const Display = () => {
+    // TODO: display Xs and Os
+    // TODO: display reset option
+    // TODO: display alert
+  }
+
+  const Game = () => {
+    let currentPlayer;
+    let turnsTotal = 0;
+    const x = Player('x');
+    const o = Player('o');
+    const view = Display();
+
+    // players keep their scores, unless the entire game/page is refreshed
+    const play = () => {
+      turnsTotal = 0;
+      currentPlayer = x;
+    };
+
+    const reset = () => { play(); }
+
+    const switchPlayer = () => {
+      turnsTotal += 1;
+      currentPlayer === x ? currentPlayer = o : currentPlayer = x;
+    }
+
+    // TODO:
+    const detectWin = () => {}
+
+    return {
+      play
+    }
+  }
+
+  const newGame = Game();
+  newGame.play();
+})();
 
